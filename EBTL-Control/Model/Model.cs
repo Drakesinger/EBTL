254adb744ad7cc2c4cef96063b40b9d524396fd7
Horia Mut <horia.mut@net2000.ch>
2016-05-01 21:00:49 +0200

    Add SQLLite nuget.

---
+++ EBTL-Control/Model/Model.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Data.Entity;
+using System.Collections.Generic;
+using Windows.Devices.Geolocation;
+
+namespace EBLT_Control
+{
+    public class EBTLContext : DbContext
+    {
+        public DbSet<Donor> Blogs { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Filename=EBTLDonors.db");
+        }
+
+        // Will see
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+
+    public class Donor
+    {
+        public string Surname { get; private set; }
+        public string Name { get; private set; }
+        public string Address { get; private set; }
+        public string BloodType { get; private set; }
+        public Geoposition GeoLocation { get; set; }
+
+        // We need a simpler object for geopositioning.
+        public Geopoint GeoPoint { get; set; }
+
+        public string EmergencyNumber { get; private set; }
+    }
+}
